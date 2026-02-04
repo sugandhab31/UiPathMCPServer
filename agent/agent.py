b14b7fd50b00fc6bd49b200599622b3eeb4a6e48
sugandhab31 <sugandhabansal31@gmail.com>
2026-02-04 20:25:44 +0530

    created Job details method

--- agent/agent.py
+++ agent/agent.py
@@ -26,7 +26,7 @@ class TokenManager:
                 "grant_type": "client_credentials",
                 "client_id": client_id,
                 "client_secret": client_secret,
-                "scope": "OR.Jobs.Read OR.Robots",
+                "scope": "OR.Default OR.Monitoring OR.Jobs OR.Robots OR.Folders"
             }
             headers = {
                 "Content-Type": "application/x-www-form-urlencoded"
@@ -41,22 +41,83 @@ class TokenManager:
         except Exception as e:
             raise e
 
-def get_job_details(access_token:str, job_id: int, top: int = 5):
-    url = "cloud.uipath.com/suganfluwktp/Sugandha/orchestrator_/odata/RobotLogs"
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Accept": "application/json"
-    }
-    params = {
+class ToolService:
+    def __init__(self, token_provider):
+        self.token_provider = token_provider
+        self.url = "https://cloud.uipath.com/suganfluwktp/Sugandha/orchestrator_"
+        self.folderID = "5165358"
+
+    def get_job_details(self, job_id: str, top: int = 2):
+        access_token = self.token_provider.get_access_token()
+        url = f"{self.url}/odata/Jobs({job_id})"        
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Accept": "application/json",
+            "X-UIPATH-OrganizationUnitId": self.folderID
+        }
+        params = {
+            "$filter": f"JobId eq {job_id}",
+            "$top": top,
+            "$orderby": "TimeStamp desc"
+        }
+        response =  requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_Jobs(self, process_name:str, state: str):
+        access_token = self.token_provider.get_access_token() 
+        url = f"{self.url}/odata/Jobs"
+
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Accept": "application/json",
+            "X-UIPATH-OrganizationUnitId": self.folderID
+        }
+
+        params = {
+            "$filter": f"ReleaseName eq '{process_name}' and State eq '{state}'",
+            "$orderby": "EndTime desc",
+            "$top": 1
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        jobs = response.json()
+
+        if not jobs:
+            return "No jobs found."
+        print(response.status_code)
+        return jobs["value"][0]["Id"]
+
+    def get_Job_Logs(self, top:int, job_id:str):
+        access_token = self.token_provider.get_access_token()
+        url = f"{self.url}/odata/RobotLogs"
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Accept": "application/json",
+            "X-UIPATH-OrganizationUnitId": self.folderID
+        }
+
+        params = {
         "$filter": f"JobId eq {job_id}",
-        "$top": top,
-        "$orderby": "TimeStamp desc"
-    }
-    response =  requests.get(url=url, headers=headers, params=params)
-    response.raise_for_status()
-    return response.json()["value"]
+        "$orderby": "TimeStamp desc",
+        "$top": top
+        }
 
-tokenManager = TokenManager()
-token = tokenManager.get_access_token()
-print(token)
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
 
+        return response.json().get("value", [])
+
+
+
+
+token = TokenManager()
+obj = ToolService(token_provider=token)
+jobid = obj.get_Jobs("DummyProcess","Faulted")
+print(jobid)
+job_details = obj.get_job_details(job_id=jobid)
+# print(job_details)
+logs = obj.get_Job_Logs(job_id=jobid, top=1)
+print(logs)
